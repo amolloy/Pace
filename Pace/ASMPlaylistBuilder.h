c2e31f08c3cda23d539e63e5195e40fe4a0366cd
Andrew Molloy <amolloy@gmail.com>
2014-05-26 09:52:07 -0400

    A very basic start to the playlist builder.

---
+++ Pace/ASMPlaylistBuilder.h
@@ -0,0 +1,13 @@
+//
+//  ASMPlaylistBuilder.h
+//  Pace
+//
+//  Created by Andrew Molloy on 5/26/14.
+//  Copyright (c) 2014 Andrew Molloy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASMPlaylistBuilder : NSObject
+-(void)buildPlaylist;
+@end
---
+++ Pace/ASMPlaylistBuilder.m
@@ -0,0 +1,30 @@
+//
+//  ASMPlaylistBuilder.m
+//  Pace
+//
+//  Created by Andrew Molloy on 5/26/14.
+//  Copyright (c) 2014 Andrew Molloy. All rights reserved.
+//
+
+#import "ASMPlaylistBuilder.h"
+#import "ASMTrack.h"
+
+static const NSInteger sTEMPTargetBPM = 153;
+static const NSInteger sTEMPVariance = 5;
+
+@implementation ASMPlaylistBuilder
+
+-(void)buildPlaylist
+{
+	NSArray* tracks = [ASMTrack instancesWhere:@"tempo NOT NULL and tempo BETWEEN ? AND ?", @(sTEMPTargetBPM - sTEMPVariance), @(sTEMPTargetBPM + sTEMPVariance)];
+
+	printf("Artist\tAlbum\tName\n");
+
+	[tracks enumerateObjectsUsingBlock:^(ASMTrack* track, NSUInteger idx, BOOL *stop) {
+		printf("\"%s\"\t\"%s\"\t\"%s\"\n", [track.artist cStringUsingEncoding:NSUTF8StringEncoding],
+			   [track.album cStringUsingEncoding:NSUTF8StringEncoding],
+			   [track.title cStringUsingEncoding:NSUTF8StringEncoding]);
+	}];
+}
+
+@end
