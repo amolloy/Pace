fbe097b56b187149ee0b73a0198c7e0c065b55d7
Andrew Molloy <amolloy@gmail.com>
2015-07-13 17:19:46 -0400

    Update App Delegate to Swift 2.0.

--- Pace/AppDelegate.swift
+++ Pace/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 
-	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool
+	{
 		self.setupFCModel()
 
 		// TESTING
@@ -27,32 +28,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		return true
 	}
 
-	func applicationWillResignActive(application: UIApplication) {
+	func applicationWillResignActive(application: UIApplication)
+	{
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 	}
 
-	func applicationDidEnterBackground(application: UIApplication) {
+	func applicationDidEnterBackground(application: UIApplication)
+	{
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 	}
 
-	func applicationWillEnterForeground(application: UIApplication) {
+	func applicationWillEnterForeground(application: UIApplication)
+	{
 		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 	}
 
-	func applicationDidBecomeActive(application: UIApplication) {
+	func applicationDidBecomeActive(application: UIApplication)
+	{
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 	}
 
-	func applicationWillTerminate(application: UIApplication) {
+	func applicationWillTerminate(application: UIApplication)
+	{
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 		// Saves changes in the application's managed object context before the application terminates.
 	}
 
 	// #pragma mark - FC Model
 
-	func setupFCModel() {
+	func setupFCModel()
+	{
 		let dbURL = self.applicationDocumentsDirectory.URLByAppendingPathComponent("paceFC.sqlite3")
 		let dbPath = dbURL.path;
 
@@ -62,7 +69,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		NSFileManager.defaultManager().copyItemAtURL(seedURL, toURL: dbURL, error: &anError)
 		*/
 
-		FCModel.openDatabaseAtPath(dbPath) { (db : FMDatabase!, schemaVersionPtr : CMutablePointer<CInt>) -> Void in
+		FCModel.openDatabaseAtPath(dbPath) { (db : FMDatabase!, schemaVersionPtr : UnsafeMutablePointer<Int32>) -> Void in
 			if db.beginTransaction()
 			{
 				// My custom failure handling. Yours may vary.
@@ -73,23 +80,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 					var lastErrorMessage = "no message"
 					if let msg = oLastErrorMessage
 					{
-						lastErrorMessage = oLastErrorMessage
+						lastErrorMessage = msg
 					}
 					//#endif
 					db.rollback()
-					// TODO Could not get the compiler to accept this interpolated string
-					//assert(0, "Migration statement \(statement) failed, code \(lastErrorCode): \(lastErrorMessage)")
+
+					assert(false, "Migration statement \(statement) failed, code \(lastErrorCode): \(lastErrorMessage)")
 					assert(false, "Migration statement failed")
 				}
 
-				var schemaVersion : CInt = -1
-				schemaVersionPtr.withUnsafePointer() {
-					schemaVersion = $0.memory
-				}
+				let schemaVersion = schemaVersionPtr[0]
 
 				if schemaVersion < 1
 				{
-					var sql = "CREATE TABLE ASMTrack (" +
+					let sql = "CREATE TABLE ASMTrack (" +
 					"    id           INTEGER PRIMARY KEY," +
 					"    duration     REAL NOT NULL," +
 					"    tempo        REAL," +
@@ -117,9 +121,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 					{
 						failedAt(3)
 					}
-					schemaVersionPtr.withUnsafePointer() {
-						$0.memory = 2
-					}
+					schemaVersionPtr[0] = 2
 				}
 
 				if schemaVersion < 3
@@ -129,9 +131,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 					{
 						failedAt(4)
 					}
-					schemaVersionPtr.withUnsafePointer() {
-						$0.memory = 3
-					}
+					schemaVersionPtr[0] = 3
 				}
 
 				db.commit()
